feecdba09ff41bdaedba5151fe6555e200809f2b
julee kim <summerbreeze0323@gmail.com>
2021-05-06 18:06:40 +0900

    mongo url key name

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(cookieParser())
 
-console.log('mongo uri: ', process.env.MONGO_URI)
+console.log('mongo uri: ', config.MONGO_URI)
 
 const mongoose = require('mongoose')
 mongoose.connect(config.MONGO_URI, {
