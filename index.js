f7561e1302a8d2f974daa2e322db120b7321b3cb
julee kim <summerbreeze0323@gmail.com>
2021-05-06 15:45:11 +0900

    cors옵션 수정

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const config = require('./config/key')
 const dotenv = require('dotenv')
 dotenv.config();
 
-app.use(cors({ credentials: true }));
+app.use(cors());
 // application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }))
 // application/json
