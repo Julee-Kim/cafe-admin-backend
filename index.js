2e065f3844180c0f5b53ec3174c85606e4e2981d
julee kim <summerbreeze0323@gmail.com>
2021-05-06 23:44:57 +0900

    .

--- index.js
+++ index.js
@@ -14,9 +14,10 @@ console.log('process.env.NODE_ENV ==> ', process.env.NODE_ENV)
 
 // app.use(cors())
 app.use(cors({
-	origin: process.env.NODE_ENV === 'production'
-	? 'https://6094001ae459cf000726bc1e--pensive-roentgen-8770e3.netlify.app'
-	: 'http://localhost:8080',
+	// origin: process.env.NODE_ENV === 'production'
+	// ? 'https://6094001ae459cf000726bc1e--pensive-roentgen-8770e3.netlify.app'
+	// : 'http://localhost:8080',
+	origin: true,
 	credentials: true,
 	exposedHeaders: ["set-cookie"],
 }));
